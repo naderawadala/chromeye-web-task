04c9a7013a99afbb61d4d2840c2581b5edad073b
Nader Awadala <naderawadala@gmail.com>
2023-03-24 10:10:46 +0200

    implement header, tba responsive fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../../data/data.json'
-
-const menu = data.main_menu
+import './header.css'
 
 const Header = () => {
+    const menu = data.main_menu
+
+    const [showNav, toggleShowNav] = useState(false)
+    const [activeNav, setActiveNav] = useState("/")
 
     return (
-        <div className='container'>
-            <div>
+        <header className='container'>
+            <nav>
+                {/* can't get svg to work <svg width="50px" height="50px" xmlns={data.company_data.logo}></svg> */}
+                <img src={data.company_data.logo} />
+                <div className={showNav ? "nav_menu show-menu" : "nav_menu"}>
+                    <ul className='nav_list grid'>
+                        {Object.keys(menu).map(
+                            (value, index) => menu[value].url != null && (
+                                <li key={index} className="nav_item">
+                                    <a onClick={() => setActiveNav(menu[value].url)}
+                                        href={menu[value].url}
+                                        className={
+                                            activeNav === menu[value].url ? "nav_link active-link" : "nav_link"
+                                        }>
+                                        {menu[value].name}
+                                    </a>
+                                </li>
+                            )
+                        )}
+                    </ul>
 
-            </div>
-            <div>
-                <ul>
-                    {Object.keys(menu).map(
-                        (value, index) => menu[value].url != null && (
-                            <li key={index} className="nav-item active">
-                                <a className="nav-link" href={menu[value].url}>
-                                    {menu[value].name}{" "}
-                                </a>
-                            </li>)
-                    )}
-                </ul>
-            </div>
-        </div>
+                    <i className="uil uil-times nav_close"
+                        onClick={() => toggleShowNav(!showNav)}>
+                    </i>
+                </div>
+
+                <div className="nav_toggle"
+                    onClick={() => toggleShowNav(!showNav)}>
+                    <i className="uil uil-apps"></i>
+                </div>
+            </nav>
+        </header>
     )
 }
 
